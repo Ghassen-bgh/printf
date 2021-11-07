e53df98809875e5abe88f9cba7e8fe0638ad27d7
Khaledxab <khaledxab@gmail.com>
2021-11-07 12:30:11 +0100

    Main _printf file

---
+++ _printf.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _printf - prints formatted data to stdout
+ * @format: string that contains the format to print
+ * Return: number of characters written
+ */
+int _printf(const char *format, ...)
+{
+
+
+}
