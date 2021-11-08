b5189eea02a06a4805330233d04e657fab4be82a
Ghassen-bgh <3591@holbertonschool.com>
2021-11-08 14:19:59 +0100

    print

--- main.h
+++ main.h
@@ -26,6 +26,7 @@ int _putchar(char c);
 int _print_char(va_list);
 int _print_string(va_list);
 int _print_percent(va_list);
+int print(const char *format, conv_t funcs[], va_list args);
 
 
 #endif
